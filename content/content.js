80bc737e52dc77f88612977555007e4fb25cc0fd
Nirmal Ram <nirmal@mozark.ai>
2024-06-10 16:43:49 +0530

    NFR-03 Fourth commit added

--- content/content.js
+++ content/content.js
@@ -3,5 +3,10 @@ const commits = [
         message : "First Commit",
         owner : "developer 0",
         date : "2024-06-10T04:55+05:30"
-    }
+    },
+    {
+        message : "Fourth Commit",
+        owner : "developer 2",
+        date : "2024-06-10T04:55+05:30"
+    },
 ]
